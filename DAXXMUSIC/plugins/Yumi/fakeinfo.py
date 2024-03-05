ce21d95118ea5dd39f3ee6633c635b0ecb7a6c3f
Golu jha <121981620+golujha9936@users.noreply.github.com>
2024-03-05 12:08:47 +0530

    Update fakeinfo.py

--- DAXXMUSIC/plugins/Yumi/fakeinfo.py
+++ DAXXMUSIC/plugins/Yumi/fakeinfo.py
@@ -61,28 +61,7 @@ async def address(_, message):
 
        """
 
-random_user_api_url = 'https://randomuser.me/api/'
 
-
-@app.on_message(filters.command("fake", prefixes="/"))
-def generate_fake_user_by_country(client, message):
-    country_name = message.text.split("/fake ", maxsplit=1)[1]
-    
-    # Call the RandomUser API to get fake user information for the specified country
-    response = requests.get(f'{random_user_api_url}?nat={country_name}')
-    
-    if response.status_code == 200:
-        user_info = response.json()['results'][0]
-        # Extract user details
-        first_name = user_info['name']['first']
-        last_name = user_info['name']['last']
-        email = user_info['email']
-        country = user_info['location']['country']
-        state = user_info['location']['state']
-        city = user_info['location']['city']
-        street = user_info['location']['street']['name']
-        zip_code = user_info['location']['postcode']
-        # Reply with the generated fake user information for the specified country
-        message.reply_text(f"𝗡𝗔𝗠𝗘➪ {first_name} {last_name}\n\n𝗘𝗠𝗔𝗜𝗟➪ {email}\n\n𝗖𝗢𝗨𝗡𝗧𝗥𝗬➪ {country}\n\n𝗦𝗧𝗔𝗧𝗘➪ {state}\n\n𝗖𝗜𝗧𝗬: {city}\n\n𝗔𝗗𝗗𝗥𝗘𝗦𝗦:➪{street}\n\n𝗭𝗜𝗣 𝗖𝗢𝗗𝗘➪ {zip_code}")
+        await message.reply_photo(photo=picture_url, caption=caption)
     else:
-        message.reply_text(f"Failed to generate fake user information for {country_name}.")
+        await message.reply_text("ᴏᴏᴘs ɴᴏᴛ ғᴏᴜɴᴅ ᴀɴʏ ᴀᴅᴅʀᴇss.")
