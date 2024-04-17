b383ca3356ac338870c3094a76a084bfeffbcf2e
Evo^xᴅ 𓆩🇮🇳𓆪 <121981620+golujha9936@users.noreply.github.com>
2024-04-18 03:13:49 +0530

    Update shayari.py

--- DAXXMUSIC/plugins/bot/shayari.py
+++ DAXXMUSIC/plugins/bot/shayari.py
@@ -64,7 +64,7 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/About_EvoXpro_Owner")
+                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/Redzone_Networks")
                     
                 ]
             ]
@@ -82,7 +82,7 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/About_EvoXpro_Owner")
+                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/Redzone_Networks")
                     
                 ]
             ]
