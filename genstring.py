4bbfa3eb54056093afd455fdcb1b2454da8f5180
Noshow-off <167023022+Noshow-off@users.noreply.github.com>
2024-04-19 02:35:39 +0530

    Update genstring.py

--- genstring.py
+++ genstring.py
@@ -13,11 +13,11 @@ i = c(":memory:", api_id=API_ID, api_hash=API_HASH)
 async def main():
     await i.start()
     ss = await i.export_session_string()
-    xx = f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ sᴛʀɪɴɢ sᴇssɪᴏɴ ᴄᴏᴘʏ ɪᴛ..\n\n`{ss}`\n\nɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ʟɪʟʏ x ᴍᴜsɪᴄ"
+    xx = f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ sᴛʀɪɴɢ sᴇssɪᴏɴ ᴄᴏᴘʏ ɪᴛ..\n\n`{ss}`\n\nɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ɴɪᴋᴋɪ x ᴍᴜsɪᴄ"
     ok = await i.send_message("me", xx)
     print("\nʜᴇʀᴇ ɪs ʏᴏᴜʀ sᴛʀɪɴɢ sᴇssɪᴏɴ, ᴄᴏᴘʏ ɪᴛ, ᴅᴏɴ'ᴛ sʜᴀʀᴇ..\n")
     print(f"\n{ss}\n") 
-    print("\nɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ʟɪʟʏ x ᴍᴜsɪᴄ\n")
+    print("\nɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ɴɪᴋᴋɪ x ᴍᴜsɪᴄ\n")
 
 
 asyncio.run(main())
