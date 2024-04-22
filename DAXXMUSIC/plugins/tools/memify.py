b16e5f9767b6a839258529e08563a41683bdba2d
Noshow-off <167023022+Noshow-off@users.noreply.github.com>
2024-04-22 22:41:54 +0530

    Update memify.py

--- DAXXMUSIC/plugins/tools/memify.py
+++ DAXXMUSIC/plugins/tools/memify.py
@@ -52,7 +52,7 @@ async def drawText(image_path, text):
         fnt = "ariel.ttf"
 
     else:
-        fnt = "./MukeshRobot/resources/default.ttf"
+        fnt = "./DAXXMUSIC/assets/default.ttf"
 
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
 
