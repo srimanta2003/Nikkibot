0db7ecd4d11a62f7f3cd0af093c66e23fe8d6a77
Noshow-off <167023022+Noshow-off@users.noreply.github.com>
2024-04-29 01:27:04 +0530

    Update userid.py

--- DAXXMUSIC/plugins/tools/userid.py
+++ DAXXMUSIC/plugins/tools/userid.py
@@ -12,8 +12,8 @@ async def getid(client, message):
     message_id = message.id
     reply = message.reply_to_message
 
-    text = f"**[ᴍᴇssᴀɢᴇ ɪᴅ:]({message.link})** `{message_id}`\n"
-    text += f"**[ʏᴏᴜʀ ɪᴅ:](tg://user?id={your_id})** `{your_id}`\n"
+    text = f"**[мѕg ι∂:]({message.link})** `{message_id}`\n"
+    text += f"**[уσυя ι∂:](tg://user?id={your_id})** `{your_id}`\n"
 
     if not message.command:
         message.command = message.text.split()
@@ -25,7 +25,7 @@ async def getid(client, message):
         try:
             split = message.text.split(None, 1)[1].strip()
             user_id = (await client.get_users(split)).id
-            text += f"**[ᴜsᴇʀ ɪᴅ:](tg://user?id={user_id})** `{user_id}`\n"
+            text += f"**[υι∂:](tg://user?id={user_id})** `{user_id}`\n"
 
         except Exception:
             return await message.reply_text("ᴛʜɪs ᴜsᴇʀ ᴅᴏᴇsɴ'ᴛ ᴇxɪsᴛ.", quote=True)
@@ -37,15 +37,15 @@ async def getid(client, message):
         and not message.forward_from_chat
         and not reply.sender_chat
     ):
-        text += f"**[ʀᴇᴘʟɪᴇᴅ ᴍᴇssᴀɢᴇ ɪᴅ:]({reply.link})** `{reply.id}`\n"
-        text += f"**[ʀᴇᴘʟɪᴇᴅ ᴜsᴇʀ ɪᴅ:](tg://user?id={reply.from_user.id})** `{reply.from_user.id}`\n\n"
+        text += f"**[яєρℓує∂ мѕg ι∂:]({reply.link})** `{reply.id}`\n"
+        text += f"**[яєρℓує∂ υι∂:](tg://user?id={reply.from_user.id})** `{reply.from_user.id}`\n\n"
 
     if reply and reply.forward_from_chat:
-        text += f"ᴛʜᴇ ғᴏʀᴡᴀʀᴅᴇᴅ ᴄʜᴀɴɴᴇʟ, {reply.forward_from_chat.title}, ʜᴀs ᴀɴ ɪᴅ ᴏғ `{reply.forward_from_chat.id}`\n\n"
+        text += f"тнє ƒσяωαя∂є∂ ¢нαηηєℓ, {reply.forward_from_chat.title}, нαѕ αη ι∂ σƒ `{reply.forward_from_chat.id}`\n\n"
         print(reply.forward_from_chat)
 
     if reply and reply.sender_chat:
-        text += f"ɪᴅ ᴏғ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴄʜᴀᴛ/ᴄʜᴀɴɴᴇʟ, ɪs `{reply.sender_chat.id}`"
+        text += f"ι∂ σƒ тнє яєρℓує∂ ¢нαт/¢нαηηєℓ, ιѕ `{reply.sender_chat.id}`"
         print(reply.sender_chat)
 
     await message.reply_text(
